94136b47ece0f31730da8fd67a186911036cace2
Michele Cannella <michele.cannella@cannellasoft.eu>
2018-09-24 22:54:08 +0200

    added colors to message and input text border

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,17 +8,24 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
   $scope.message = "";
+  $scope.messageStyle = "";
   $scope.lunchMenuItems = "";
+  $scope.borderColor = "";
 
   $scope.updateMessage = function() {
     var numItems = countLunchItems($scope.lunchMenuItems);
 
-    if (numItems === 0)
-      $scope.message = "Please enter data first"
+    if (numItems === 0) {
+      $scope.message = "Please enter data first";
+      $scope.messageStyle = "color:red";
+      $scope.borderColor = "border-color: red";
+    }
     else {
+      $scope.messageStyle = "color:green";
+      $scope.borderColor = "border-color: green";
       if (numItems <= 3)
         $scope.message = "Enjoy!";
-    else
+      else
         $scope.message = "Too much!";
     }
   };
